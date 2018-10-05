dd514587c0207818b6dea828870754bc894280a9
efzet <fauzi.padlaw@gmail.com>
2018-10-06 06:09:53 +0700

    moved route for register into middleware auth group router

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-$router->post('register', 'UserController@register');
 $router->post('login', 'AuthController@login');
 
 $router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->post('users/register', 'UserController@register');
     $router->get('users', 'UserController@index');
 });
